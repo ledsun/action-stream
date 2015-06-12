99052ba1097a45e3be326b5841a817e29062f9e7
ledsun <shigeru.nakajima@gmail.com>
2015-06-12 17:23:25 +0900

    Include defaultOption.json.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 
-gulp.task("default", function() {
+gulp.task("babel", function() {
   return gulp.src("src/**/*.js", {
       base: "src"
     })
     .pipe(babel())
     .pipe(gulp.dest("."));
 });
+
+gulp.task("copy", function() {
+  return gulp.src("src/lib/defaultOption.json", {
+      base: "src"
+    })
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("default", ["babel", "copy"])
