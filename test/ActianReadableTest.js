57f4c7b4da2276f21642588ad6e7c1a1e94d849e
ledsun <shigeru.nakajima@gmail.com>
2015-06-15 12:05:29 +0900

    Add a test code of the ActionReadable.

---
+++ test/ActianReadableTest.js
@@ -0,0 +1,47 @@
+import assert from 'power-assert'
+import {
+  Writable
+}
+from 'stream'
+import {
+  ActionReadable
+}
+from '../src'
+
+class ActionReadableSample extends ActionReadable {
+  constructor(selector) {
+    super(selector)
+    this.name = 'ActionReadableSample'
+  }
+  _bindComponent(selector, push) {
+    this.onClick = action => push(action)
+  }
+  click(action) {
+    this.onClick(action)
+  }
+}
+
+describe('ActionReadable', () => {
+  it('is able to pipe a Writable', (mochaDone) => {
+    let sampleAction = {
+      a: 'a'
+    }
+
+    class WritableStub extends Writable {
+      constructor() {
+        super({
+          "objectMode": true
+        })
+      }
+      _write(chunk, encoding, done) {
+        assert(chunk, sampleAction, 'an action is changed')
+        done()
+        mochaDone()
+      }
+    }
+
+    let r = new ActionReadableSample()
+    r.pipe(new WritableStub())
+    r.click(sampleAction)
+  })
+})
