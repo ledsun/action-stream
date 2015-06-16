1e10d855a8ad0fa6f1be157312089546fe92d160
ledsun <shigeru.nakajima@gmail.com>
2015-06-16 11:17:48 +0900

    Add test codes for the ActionTransform.

--- test/ActionTransformTest.js
+++ test/ActionTransformTest.js
@@ -16,6 +16,17 @@ describe('ActionTransform', () => {
     type: 'some'
   }
 
+  class ReadableDriver extends Readable {
+    constructor() {
+      super({
+        "objectMode": true
+      })
+
+      this.push(sampleAction)
+    }
+    _read() {}
+  }
+
   it('is pipe from Readable', (mochaDone) => {
     class ActionTransformSample extends ActionTransform {
       constructor() {
@@ -25,17 +36,6 @@ describe('ActionTransform', () => {
         assert.equal(action, sampleAction)
         mochaDone()
       }
-    }
-
-    class ReadableDriver extends Readable {
-      constructor() {
-        super({
-          "objectMode": true
-        })
-
-        this.push(sampleAction)
-      }
-      _read() {}
     }
 
     new ReadableDriver().pipe(
@@ -58,17 +58,6 @@ describe('ActionTransform', () => {
   })
 
   it('is able to pipe a Writable', (mochaDone) => {
-    class ReadableDriver extends Readable {
-      constructor() {
-        super({
-          "objectMode": true
-        })
-
-        this.push(sampleAction)
-      }
-      _read() {}
-    }
-
     class ActionTransformSample extends ActionTransform {
       constructor() {
         super()
@@ -96,6 +85,70 @@ describe('ActionTransform', () => {
     new ReadableDriver()
       .pipe(new ActionTransformSample())
       .pipe(new WritableStub())
+  })
 
+  it('is able to push anothor action', (mochaDone) => {
+    class PushOptionTransform extends ActionTransform {
+      constructor() {
+        super()
+        this.name = 'PushOptionTransform'
+      }
+      _transformAction(action, push) {
+        push({
+          option: 'option1'
+        })
+        push({
+          option: 'option2'
+        })
+      }
+    }
+
+    class WritableStub extends Writable {
+      constructor() {
+        super({
+          "objectMode": true
+        })
+        this.count = 0
+      }
+      _write(chunk, encoding, done) {
+        if (this.count === 0) {
+          assert.notEqual(chunk, sampleAction, 'an original action is not changed')
+          assert.equal(chunk.target, sampleAction.target)
+          assert.equal(chunk.type, sampleAction.type)
+          assert.equal(chunk.source, 'ReadableDriver')
+          this.count++;
+          done()
+          return;
+        }
+
+        if (this.count === 1) {
+          // Remain original values.
+          assert.equal(chunk.target, sampleAction.target)
+          assert.equal(chunk.type, sampleAction.type)
+          // Add source.
+          assert.deepEqual(chunk.source, ['ReadableDriver', 'PushOptionTransform'])
+          assert.equal(chunk.option, 'option1')
+          this.count++;
+          done()
+          return;
+        }
+
+        if (this.count === 2) {
+          // Push multiple actions.
+          assert.equal(chunk.target, sampleAction.target)
+          assert.equal(chunk.type, sampleAction.type)
+          assert.deepEqual(chunk.source, ['ReadableDriver', 'PushOptionTransform'])
+          assert.equal(chunk.option, 'option2')
+          this.count++;
+          done()
+        }
+
+        mochaDone()
+      }
+    }
+
+    new ReadableDriver()
+      .pipe(new PushOptionTransform())
+      .pipe(new WritableStub())
   })
 })
